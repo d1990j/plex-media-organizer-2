4a2ac37c9a73c911aa613659ed7afd311252f528
Dewain Jones <dewainjones@icloud.com>
2026-07-18 10:35:55 -0500

    Begin split up of files. Fix issue of circular imports.

--- logic.py
+++ logic.py
@@ -1,8 +1,124 @@
 import flet as ft
 from state import AppState
-from ui import UI
+import os
+import ui
 
 class Logic:
-    def __init__(self, state: AppState, ui: UI):
+    def __init__(self, state: AppState):
         self.state = state
-        self.ui = ui
+
+    ##################### LOGIC ############################
+    async def browse_source_directory(self, e: ft.Event[ft.Button], ui: ui.UI):
+        """Open a dialog to select a directory"""
+        print("Browse source directory selected") # Testing
+        self.state.source_directory_path.value = await ft.FilePicker().get_directory_path() or self.state.source_directory_path.value
+        print(f"Source directory set to {self.state.source_directory_path.value}")
+        self.state.source_directory_path.update()
+        self.load_media_files(ui.file_list)
+        # toggle the commit button if a directory is chosen for source and destination
+        self.toggle_commit_button()
+
+    # async def browse_destination_directory():
+    #     """Open a dialog to select a directory"""
+    #     print("Browse destination directory selected") # testing
+    #     state.destination_directory_path.value = await ft.FilePicker().get_directory_path() or state.destination_directory_path.value
+    #     print(f"Destination directory set to {state.destination_directory_path.value}")
+    #     state.destination_directory_path.update()
+    #     # toggle the commit button if a directory is chosen for source and destination
+    #     toggle_commit_button()
+
+    # def stage_file():
+    #     """Stage selected file for naming."""
+    #     if tv_movie_switch.value: # If True, is Movie, otherwise is TV
+    #         print("Stage start for Movie file")
+    #     else:
+    #         print("Stage start for TV file")
+
+    # def commit_files():
+    #     """Commit the staged files, completing change"""
+    #     print("Commit files selected")
+
+    # def source_file_clicked(self, e: ft.Event[ft.ListTile]):
+    #     index = e.control.data
+    #     print(f"source file clicked at index {index}")
+    #     # update the selected file index
+    #     state.selected_file_index = index
+
+    #     # reload the files to update highlights
+    #     load_media_files()
+
+    #     # Fill the text fields in selected file info
+    #     fill_selected_file_fields(
+    #         state.media_files[index]["name"],
+    #         state.media_files[index]["year"],
+    #         state.media_files[index]["tv_movie"],
+    #         state.media_files[index]["season"],
+    #         state.media_files[index]["episode"]
+    #     )
+
+    # def fill_selected_file_fields(title: str, year: str, tv_movie: bool, season: str, episode: str):
+    #     title_textfield.value = title
+    #     year_textfield.value = year
+    #     tv_movie_switch.value = tv_movie
+    #     season_textfield.value = season
+    #     episode_textfield.value = episode
+    #     selected_info_container.update()
+
+    def toggle_commit_button(self):
+          print("Commit button pressed")
+
+    def load_media_files(self, file_list: ft.ListView):
+        """Attempt to load the media files from directory"""
+        # If a directory is chosen
+        if self.state.source_directory_path.value != "No path selected":
+            # Reset media files
+            self.state.media_files.clear()
+            file_list.controls.clear()
+
+            # Find each file in the chosen directory that has the correct filetype and add to media files 
+            for file in os.listdir(self.state.source_directory_path.value):
+                    if file.lower().endswith((".mp4", ".mkv", ".avi", ".mov", ".mp3", ".flac")):
+                        self.state.media_files.append({"name": file, "tv_movie": False, "new_name": "", "year": "", "season": "", "episode": ""})
+
+            # Cycle through each item in the files and add the name to the list
+            for index, item in enumerate(self.state.media_files):
+                tile = ft.ListTile(
+                        title=ft.Text(item["name"]),
+                        #on_click=self.source_file_clicked,
+                        data=index,
+                        selected_color=ft.Colors.BLUE_500
+                )
+                if index == self.state.selected_file_index:
+                    tile.selected = True
+                else:
+                    tile.selected = False
+                file_list.controls.append(tile)
+                
+                #Update the file list
+                file_list.update()
+
+    # def play_media_file():
+    #     """Attempt to play the media file"""
+    #     try:
+    #         index = state.selected_file_index
+    #         file = state.media_files[index]["name"]
+    #         filepath = os.path.join(state.source_directory_path.value, file)
+    #         print(f"Filepath created: {filepath}")
+    #         # Determine OS, nt for windows, posix for mac/linux
+    #         if os.name == "nt":
+    #             print("OS set to nt")
+    #             os.startfile(filepath)
+    #         elif os.name == "posix":
+    #             print("OS set to posix")
+    #             subprocess.run(["open", filepath])
+    #     except:
+    #         print("Unable to play media")
+
+    # def update_on_tv_movie_switch():
+    #     """Update the text boxes upon switch change"""
+    #     season_textfield.disabled = tv_movie_switch.value
+    #     episode_textfield.disabled = tv_movie_switch.value
+    #     print(f"Season textfield disabled set to: {season_textfield.disabled}")
+    #     print(f"Episode textfield disabled set to: {episode_textfield.disabled}")
+    #     season_textfield.update()
+    #     episode_textfield.update()
--- ui.py
+++ ui.py
@@ -1,5 +1,188 @@
 import flet as ft
+import logic
 
 class UI:
-    def __init__(self, page: ft.Page):
-        pass
+    def __init__(self, page: ft.Page, logic: logic.Logic):
+        self.page = page
+        self.logic = logic
+    
+        ########################## Buttons ############################
+        self.source_button = ft.Button(
+            content="Browse",
+            #on_click=browse_source_directory
+        )
+        self.destination_button = ft.Button(
+            content="Browse",
+            #on_click=browse_destination_directory
+        )
+        self.play_button = ft.Button(
+            content="Play",
+            #on_click=play_media_file
+        )
+        self.stage_button = ft.Button(
+            content="Stage",
+            #on_click=stage_file
+        )
+        self.commit_button = ft.Button(
+            content="Commit",
+            #on_click=commit_files,
+            disabled=True
+        )
+
+        ######################## Lists #############################
+        self.file_list = ft.ListView(
+            controls=[
+                #ft.Text(f"Item {i}") for i in range(1, 100) # For testing
+            ],
+            expand=True
+        )
+        self.stage_list = ft.ListView(
+            controls=[
+                ft.Text(f"Item {i}") for i in range(1, 100) # For testing
+            ],
+            expand=True
+        )
+
+        ######################### Switches ##########################
+        self.tv_movie_switch = ft.Switch(
+            label="TV/Movie", 
+            value=False,
+            #on_change=update_on_tv_movie_switch
+        )
+
+        ##################### Text Fields ###########################
+        self.title_textfield = ft.TextField(
+            label="Title"
+        )
+        self.year_textfield = ft.TextField(
+            label="Year"
+        )
+        self.season_textfield = ft.TextField(
+            label="Season",
+            col=6,
+            #disabled=tv_movie_switch.value
+        )
+        self.episode_textfield = ft.TextField(
+            label="Episode",
+            col=6,
+            #disabled=tv_movie_switch.value
+        )
+
+        ########################## Top row container ########################
+        self.top_container = ft.Container(
+            content=ft.Row(
+                [
+                    self.source_button,
+                    #state.source_directory_path
+                ]
+            ),
+            bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+            padding=20,
+            border_radius=10,
+            col=12
+        )
+
+        ######################## Middle row containers #########################
+        self.file_list_container = ft.Container(
+            bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+            padding=20,
+            border_radius=10,
+            col=4,
+            content=ft.Column(
+                [
+                    self.file_list
+                ]
+            )
+        )
+        self.selected_info_container = ft.Container(
+            bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+            padding=20,
+            border_radius=10,
+            col=4,
+            content=ft.Column(
+                [
+                    ft.Text("Media Info"),
+                    self.title_textfield,
+                    self.year_textfield,
+                    self.tv_movie_switch,
+                    ft.ResponsiveRow(
+                        [
+                            self.season_textfield,
+                            self.episode_textfield
+                        ]
+                    ),
+                    ft.Container(expand=True), # Push buttons to bottom
+                    ft.Row(
+                        [
+                            self.play_button,
+                            self.stage_button
+                        ],
+                        alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+                    )
+                ]
+            )
+        )
+        self.stage_list_container = ft.Container(
+            bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+            padding=20,
+            border_radius=10,
+            col=4,
+            content=ft.Column(
+                [
+                    self.stage_list
+                ]
+            )
+        )
+
+        ######################### Bottom row container #########################
+        self.bottom_container = ft.Container(
+            content=ft.Row(
+                [
+                    self.destination_button,
+                    #state.destination_directory_path,
+                    ft.Container(expand=True), # Add space between
+                    self.commit_button
+                ]
+            ),
+            bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+            padding=20,
+            border_radius=10,
+            col=12
+        )
+
+    
+    
+    ########################### Add items to page #############################
+    def build_page(self):
+        self.page.add(
+            ft.Column(
+                [
+                    # Top Row
+                    ft.ResponsiveRow(
+                        [
+                            self.top_container
+                        ]
+                    ),
+
+                    # Middle Row
+                    ft.ResponsiveRow(
+                        [
+                            self.file_list_container,
+                            self.selected_info_container,
+                            self.stage_list_container
+                        ],
+                        expand=True
+                    ),
+
+                    # Bottom Row     
+                    ft.ResponsiveRow(
+                        [
+                            self.bottom_container
+                        ]
+                    )   
+                ],
+                spacing=10,
+                expand=True
+            )
+            
+        )
