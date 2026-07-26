864f5e3d91e6f587ad3c9e9364aadfd7bc4ec640
Dewain Jones <dewainjones@icloud.com>
2026-07-26 16:54:13 -0500

    Create a new file containing a class for tv and movie with the goal of replacing the dictionaries.

---
+++ mediafile.py
@@ -0,0 +1,47 @@
+class Movie:
+    def __init__(self, file_name: str, new_name: str | None = None, year: str | None = None):
+        self.new_name = new_name
+        self.year = year
+        self.file_name = file_name
+        self.staged = False
+
+    def get_plex_folder_name(self) -> str:
+        """Return the name of the file folder using the Plex naming convention."""
+        plex_name = f"{self.new_name} ({self.year})" if self.year else f"{self.new_name}"
+        return plex_name
+
+    def get_plex_name(self) -> str:
+        """Return the name of the file using the Plex naming convention."""
+        plex_name = f"{self.new_name} ({self.year})" if self.year else f"{self.new_name}"
+        return plex_name
+
+class TV:
+    def __init__(self, file_name: str, new_name: str | None = None, year: str | None = None, season: str | None = None, episode: str | None = None):
+        self.new_name = new_name
+        self.year = year
+        self.season = season
+        self.episode = episode
+        self.file_name = file_name
+        self.staged = False
+
+    def get_plex_folder_series_name(self) -> str:
+        """Return the name of the file folder for the series using the Plex naming convention."""
+        plex_name = f"{self.new_name} ({self.year})" if self.year else f"{self.new_name}"
+        return plex_name
+
+    def get_plex_folder_season_name(self) -> str:
+        """Return the name of the file folder for the season using the Plex naming convention."""
+        if self.season:
+            plex_name = f"Season {self.season.zfill(2)}"
+        else:
+            print(f"ERROR: No season selected for {self.file_name}, using 0.")
+            plex_name = "0"
+        return plex_name
+
+    def get_plex_name(self) -> str:
+        """Return the name of the file using the Plex naming convention."""
+        if self.season and self.episode and self.year:
+            plex_name = f"{self.new_name} ({self.year}) - S{self.season.zfill(2)}E{self.episode.zfill(2)}"
+        else:
+            plex_name = f"{self.new_name}"
+        return plex_name
