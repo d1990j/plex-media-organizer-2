a7517563bbc6504c7a82729e29baf0c51e72b988
Dewain Jones <dewainjones@icloud.com>
2026-07-26 17:12:16 -0500

    Change media file classes from one for TV and one for Movie to one general MediaFile type that can be used for either.

--- mediafile.py
+++ mediafile.py
@@ -1,27 +1,13 @@
-class Movie:
-    def __init__(self, file_name: str, new_name: str | None = None, year: str | None = None):
-        self.new_name = new_name
-        self.year = year
-        self.file_name = file_name
-        self.staged = False
+from constants import MediaType
 
-    def get_plex_folder_name(self) -> str:
-        """Return the name of the file folder using the Plex naming convention."""
-        plex_name = f"{self.new_name} ({self.year})" if self.year else f"{self.new_name}"
-        return plex_name
-
-    def get_plex_name(self) -> str:
-        """Return the name of the file using the Plex naming convention."""
-        plex_name = f"{self.new_name} ({self.year})" if self.year else f"{self.new_name}"
-        return plex_name
-
-class TV:
-    def __init__(self, file_name: str, new_name: str | None = None, year: str | None = None, season: str | None = None, episode: str | None = None):
+class MediaFile:
+    def __init__(self, file_name: str, new_name: str | None = None, year: str | None = None, season: str | None = None, episode: str | None = None, type: MediaType | None = None):
         self.new_name = new_name
         self.year = year
         self.season = season
         self.episode = episode
         self.file_name = file_name
+        self.type = type
         self.staged = False
 
     def get_plex_folder_series_name(self) -> str:
@@ -40,8 +26,15 @@ class TV:
 
     def get_plex_name(self) -> str:
         """Return the name of the file using the Plex naming convention."""
-        if self.season and self.episode and self.year:
-            plex_name = f"{self.new_name} ({self.year}) - S{self.season.zfill(2)}E{self.episode.zfill(2)}"
+        if self.type == MediaType.TV:
+            if self.season and self.episode and self.year:
+                plex_name = f"{self.new_name} ({self.year}) - S{self.season.zfill(2)}E{self.episode.zfill(2)}"
+            else:
+                plex_name = f"{self.new_name}"
+            return plex_name
         else:
-            plex_name = f"{self.new_name}"
-        return plex_name
+            if self.year:
+                plex_name = f"{self.new_name} ({self.year})"
+            else:
+                plex_name = f"{self.new_name}"
+            return plex_name
